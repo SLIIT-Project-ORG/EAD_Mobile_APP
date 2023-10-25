ab5d2aab1d50b892f66fb6ff06c1f4d0cba5ad65
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-10-25 20:56:08 +0530

    final commit

---
+++ app/src/main/java/com/example/ead_mobile_app/Model/ResponseDto.java
@@ -0,0 +1,28 @@
+package com.example.ead_mobile_app.Model;
+
+public class ResponseDto {
+
+    private String message;
+    private String success;
+
+    public ResponseDto(String message, String success) {
+        this.message = message;
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
